7d6c8f145f406e045e1d054c4e96a833291802f6
vladwho <vladpresedinte@gmail.com>
2016-04-09 20:13:07 +0300

    Update design landing

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'ionic-material','starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ionMdInput', 'ionic-material','starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('SearchCtrl', function($log, $http, $scope, $timeout, $stateParams, ionicMaterialInk,$state) {
+.controller('SearchCtrl', function($log, $http, $scope, $timeout, $stateParams, ionicMaterialInk,$state, $ionicNavBarDelegate) {
     ionicMaterialInk.displayEffect();
     $scope.find = "Slimy";
 
@@ -11,6 +11,8 @@ angular.module('starter.controllers', [])
 })
 
 .controller('SearchResultsCtrl', function($log, $http,$scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+
+  $scope.find = $stateParams.search;
 
   $http({
     'mehod': 'GET',
@@ -35,4 +37,60 @@ angular.module('starter.controllers', [])
     //     selector: '.animate-fade-slide-in .item'
     // });
 
-});
+})
+
+.directive('searchBar', [function () {
+	return {
+		scope: {
+			ngModel: '='
+		},
+		require: ['^ionNavBar', '?ngModel'],
+		restrict: 'E',
+		replace: true,
+		template: '<ion-nav-buttons side="right">'+
+						'<div class="searchBar">'+
+							'<div class="searchTxt" ng-show="ngModel.show">'+
+						  		'<div class="bgdiv"></div>'+
+						  		'<div class="bgtxt">'+
+						  			'<input type="text" placeholder="Search" ng-model="ngModel.txt">'+
+						  		'</div>'+
+					  		'</div>'+
+						  	'<i class="icon placeholder-icon" ng-click="ngModel.txt=\'\';ngModel.show=!ngModel.show"></i>'+
+						'</div>'+
+					'</ion-nav-buttons>',
+
+		compile: function (element, attrs) {
+			var icon=attrs.icon
+					|| (ionic.Platform.isAndroid() && 'ion-android-search')
+					|| (ionic.Platform.isIOS()     && 'ion-ios-search')
+					|| 'ion-ios-search';
+			angular.element(element[0].querySelector('.icon')).addClass(icon);
+
+			return function($scope, $element, $attrs, ctrls) {
+				var navBarCtrl = ctrls[0];
+				$scope.navElement = $attrs.side === 'right' ? navBarCtrl.rightButtonsElement : navBarCtrl.leftButtonsElement;
+
+			};
+		},
+		controller: ['$scope','$ionicNavBarDelegate', function($scope,$ionicNavBarDelegate){
+			var title, definedClass;
+			$scope.$watch('ngModel.show', function(showing, oldVal, scope) {
+				if(showing!==oldVal) {
+					if(showing) {
+						if(!definedClass) {
+							var numicons=$scope.navElement.children().length;
+							angular.element($scope.navElement[0].querySelector('.searchBar')).addClass('numicons'+numicons);
+						}
+
+						title = $ionicNavBarDelegate.title();
+						$ionicNavBarDelegate.title('');
+					} else {
+						$ionicNavBarDelegate.title(title);
+					}
+				} else if (!title) {
+					title = $ionicNavBarDelegate.title();
+				}
+			});
+		}]
+	};
+}]);
